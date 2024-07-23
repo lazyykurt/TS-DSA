02c34f349e64270eabcbd7788ba8cd86e087ac52
lazyykurt <lazyyykurt@gmail.com>
2024-07-24 00:02:25 +0800

    第二次提交，完成测试但是单个字符串未通过

--- src/algorithms/解题/0003无重复字符的最长子串.ts
+++ src/algorithms/解题/0003无重复字符的最长子串.ts
@@ -1,17 +1,36 @@
 function lengthOfLongestSubstring(s: string): number {
   let result = 0;
-  let set = new Set();
+  let map = new Map();
+  let left = 0;
 
   for (let i = 0; i < s.length; i++) {
-    if(!set.has(s[i])) {
-      set.add(s[i])
+    if (!map.has(s[i])) {
+      result = Math.max(result, i - left);
+      map.set(s[i], i);
     } else {
-      result = Math.max(result, set.size);
-      set = new Set();
-      set.add(s[i])
+      result = Math.max(result, i - left);
+      left = map.get(s[i]) + 1;
+      map.set(s[i], i);
     }
   }
-  return Math.max(result, set.size);
+  return result;
 }
 
-console.log(lengthOfLongestSubstring("na73menameabbcy"))
+console.log(lengthOfLongestSubstring("pwwkew"));
+
+/*
+
+思路逻辑：
+之前的缺陷： eabdcazxw，碰到重复a，直接舍去了第二个a前面的所以字符，得到azxw，❌
+
+我们应该舍去第一个a， 前面的字符 ea
+
+如何知道第一个a的位置，我们存入 map 中。map.set(s[i], i), key 为该字符，value为该字符的索引
+
+引入 left 从而得到最大值
+当遇到一样的字符，得到left的值，为当前重复字符的next索引eabdcazxw，left -> b， map(s[i])+1
+
+因为left移动最大值会减小，在此之前，我们得更新判断一下最大值
+
+返回时再判断一次最后的最大值。
+*/
