cae69168f22319cb3fb368a9ec8798e0b35a6677
lazyykurt <lazyyykurt@gmail.com>
2024-07-24 02:01:31 +0800

    设置 swapped ，如果一轮下来没有交换，则排序完成，直接返回

--- src/algorithms/排序/冒泡排序.ts
+++ src/algorithms/排序/冒泡排序.ts
@@ -1,17 +1,23 @@
 function bubbleSort(arr: number[]): number[] {
+
+  let swapped = false;
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
+        // 交换元素
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+
+        swapped = true;
       }
     }
+    // 如果没有发生交换，则数组已经有序
+    if (!swapped) break;
   }
   return arr;
 }
 
 console.log(bubbleSort([1, 3, 2, 6, 0]));
 
-// 外层为排序的总轮数，进行 arr.length - 1 次，每次将最大数放到最前面。
-// 内层为，交换的次数，进行 arr.length - 1 - i 次交换。 
+// 通过设置 swapped，可确保提前终止代码运行。
